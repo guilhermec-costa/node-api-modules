cb435bfb66a875a0f586a6115bab75329c2debe0
Guilherme China <echina725@gmail.com>
2025-03-22 18:21:07 -0300

    refactor

---
+++ fs-api.js
@@ -0,0 +1,93 @@
+const fsP = require("node:fs").promises;
+const fsC = require("node:fs");
+
+class FsPromiseBasedApi {
+
+  /**
+   * @param {string} path - The path to unlink
+   */
+  unlinkPath(path) {
+    fsP.unlink(path)
+      .then((v) => {
+        console.log("Path unlinked");
+      })
+      .catch((err) => {
+        console.log("Failed to unlink path: ", err);
+      })
+  }
+
+  /**
+   * 
+   * @param {string} targetPath - source to symlink
+   * @param {string} linkName - link name
+   */
+  async linkPath(targetPath, linkName) {
+    try {
+      fsP.link(targetPath, linkName)
+    } catch (error) {
+      console.log("Failed to link path: ", error) 
+    }
+  }
+
+  /**
+   * @param {string} path
+   */
+  async openFile(path) {
+    try {
+      const handler = await fsP.open(path, "r");
+      await handler.writeFile("content of the file");
+      handler.close();
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  async createFile(path, content, encoder) {
+    const handler = await fsP.open(path, "w")
+    handler.writeFile(content, {
+      encoding: encoder
+    })
+    handler.close();
+  }
+
+  /**
+   * 
+   * @param {string} path 
+   * @returns {Promise<void>} 
+   */
+  async readFile(path, buffer) {
+    try {
+      const fHandler = await fsP.open(path, "r+");
+      // takes a buf reference
+      await fHandler.read(buffer, 0, 100, null);
+      fHandler.close();
+    } catch (error) {
+      console.log("Failed to read file: ", fHandler) 
+    }
+  }
+
+  async readFileWithBuffer(path) {
+    const handler = await fsP.open(path, "r+");
+    // returns a buf reference
+    const content = await handler.readFile();
+    handler.close();
+    return content; 
+  }
+
+  async loopLines(path) {
+    const handler = await fsP.open(path, "r+");
+    for await (const line of handler.readLines()) {
+      console.log(line);
+    }
+  }
+
+  async createReadStream(path) {
+    const hd = await fsP.open(path, "r+")
+    const stream = hd.createReadStream()
+    stream.on("data")
+  }
+}
+
+module.exports = {
+  FsPromiseBasedApi
+}
--- main.js
+++ main.js
@@ -1,78 +1,5 @@
-const fsP = require("node:fs").promises;
 const { Buffer } = require("node:buffer");
-const fsC = require("node:fs");
-
-class FsPromiseBasedApi {
-
-  /**
-   * @param {string} path - The path to unlink
-   */
-  unlinkPath(path) {
-    fsP.unlink(path)
-      .then((v) => {
-        console.log("Path unlinked");
-      })
-      .catch((err) => {
-        console.log("Failed to unlink path: ", err);
-      })
-  }
-
-  /**
-   * 
-   * @param {string} targetPath - source to symlink
-   * @param {string} linkName - link name
-   */
-  async linkPath(targetPath, linkName) {
-    try {
-      fsP.link(targetPath, linkName)
-    } catch (error) {
-      console.log("Failed to link path: ", error) 
-    }
-  }
-
-  /**
-   * @param {string} path
-   */
-  async openFile(path) {
-    try {
-      const handler = await fsP.open(path, "r");
-      await handler.writeFile("content of the file");
-      handler.close();
-    } catch (error) {
-      console.log(error.message)
-    }
-  }
-
-  async createFile(path, content, encoder) {
-    const handler = await fsP.open(path, "w")
-    handler.writeFile(content, {
-      encoding: encoder
-    })
-    handler.close();
-  }
-
-  /**
-   * 
-   * @param {string} path 
-   * @returns {Promise<void>} 
-   */
-  async readFile(path, buffer) {
-    try {
-      const fHandler = await fsP.open(path, "r+");
-      // takes a buf reference
-      await fHandler.read(buffer, 0, 100, null);
-      fHandler.close();
-    } catch (error) {
-      console.log("Failed to read file: ", fHandler) 
-    }
-  }
-
-  async readFileWithBuffer(path) {
-    const handler = await fsP.open(path, "r+");
-    // returns a buf reference
-    return await handler.readFile();
-  }
-}
+const {FsPromiseBasedApi} = require("./fs-api.js");
 
 const instance = new FsPromiseBasedApi();
 instance.linkPath("./main.js", "newMainLink.js");
@@ -90,3 +17,5 @@ instance.readFile("./testfile", buf).then(() => {
 instance.readFileWithBuffer("./testfile").then((c) => {
   console.log("content: ", c.toString())
 })
+
+instance.loopLines("./testfile")
